6d1fcf9a97a237aea30b94d90e929ffb5ba039c1
Dmitry <86959216+DmitryPash@users.noreply.github.com>
2024-12-23 17:06:05 +0300

    добавлена проверка на знак минус

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 (() => {
-    const D = document;
+  const D = document;
 
-    const setArea = D.querySelector('#setArea'),
-          getArea = D.querySelector('#getArea'),
-          res = D.querySelector('#result');
+  const setArea = D.querySelector("#setArea"),
+    getArea = D.querySelector("#getArea"),
+    res = D.querySelector("#result");
 
+  const translate = () => {
+    const result = setArea.value
+      .replace(/((?!1px)-?\d+(\.\d+)?px?(\s+||$))+/g, (match) => {
+        const PX = match.match(/-?\d+(\.\d+)?/g);
 
-    const translate = () => {
-        const result = setArea.value.replace(/((?!1px)\d+(\.\d+)?px?(\s+||$))+/g, (match) => {
+        return `em(${PX.join(" ")}, $fz)`;
+      })
+      .replace(/font-size: em\((\d+),\s*\$fz\)/g, (match) => {
+        const NUM = match.match(/\d+/g);
 
-            const PX = match.match(/\d+(\.\d+)?/g)
+        return ` $fz: ${NUM};
+                font-size: em($fz, $fz_base)`;
+      })
+      .replace(/\$fz\)(?!;)+/g, "$fz) ");
 
-            return `em(${PX.join(' ')}, $fz)`;
+    getArea.innerHTML = result;
+  };
 
-        }).replace(/font-size: em\((\d+),\s*\$fz\)/g, (match) => {
-
-            const NUM = match.match(/\d+/g)
-
-            return ` $fz: ${NUM};
-                font-size: em($fz, $fz_base)`
-
-        }).replace(/\$fz\)(?!;)+/g, '$fz) ');
-
-        getArea.innerHTML = result
-    }
-
-
-    res.addEventListener('click', translate)
-})()
+  res.addEventListener("click", translate);
+})();
